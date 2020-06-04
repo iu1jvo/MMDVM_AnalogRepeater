c7882d813c39233d1655079fdc947991489b8b37
IU1JVO <iu1jvo@gmail.com>
2020-06-04 11:46:45 +0200

    MMDVM_AnalogRepeater: firmware to activate Audio and PTT relais if the RX radio recognize a subtone and activate the subtone presence output. if the subtone is absent, the output are deactivared after a little time (relais queue).

---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+
+/**
+ * @brief Define Used Pins
+ * 
+ */
+#define INP_SIGNAL  PIN2
+#define OUT_PTT     PIN3
+#define OUT_AUDIO   PIN4
+
+/**
+ * @brief Define Timings
+ * 
+ */
+#define DEACTIVE_PTT_MS 250
+#define DEACTVE_AUDIO_MS 350
+
+/**
+ * @brief TimeExpired: compute if (interval) is elapsed form (start) to millis().
+ * 
+ * @param start starting time
+ * @param interval interval of time to wating from.
+ * @return true if time interval is elapsed
+ * @return false if time interval is NOT elapsed
+ */
+bool TimeExpired(unsigned long start, unsigned long interval)
+{
+    if ((millis() - start) >= interval)
+      return true;
+
+    return false; 
+}
+
+
+void setup() {
+  /**
+   * @brief Setup Pin Mode
+   * 
+   */
+  pinMode(INP_SIGNAL, INPUT_PULLUP);
+  pinMode(OUT_AUDIO, OUTPUT);
+  pinMode(OUT_PTT, OUTPUT);
+  /**
+   * @brief Start Output state
+   * 
+   */
+  digitalWrite(OUT_AUDIO, false);
+  digitalWrite(OUT_PTT, false);
+
+
+}
+
+void loop() {
+  static unsigned long StartWait = 0;
+  /**
+   * @brief Read Signal: beware the signal is inverted!
+   * if signal is present, activate output.
+   * 
+   */
+  if (digitalRead(INP_SIGNAL) == 0) {
+    StartWait = millis();
+    digitalWrite(OUT_AUDIO, true);
+    digitalWrite(OUT_PTT, true);
+  }
+  else {
+  /**
+   * @brief No siglal, deactivate outpur after the expected time.
+   * 
+   */
+    if (TimeExpired(StartWait, DEACTIVE_PTT_MS)) 
+      digitalWrite(OUT_PTT, false);
+    
+    if (TimeExpired(StartWait, DEACTVE_AUDIO_MS)) 
+      digitalWrite(OUT_AUDIO, false);  
+  }
+}
